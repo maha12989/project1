ef6841613c26cbaa92987e8d582ae87e5ae7e3c9
mahadi.12989 <mahadihasan12989@gmail.com>
2024-04-22 10:47:55 +0600

    Model add

--- denv/teacher/admin.py
+++ denv/teacher/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blood_group
 
 # Register your models here.
+admin.site.register(Blood_group)
--- denv/teacher/models.py
+++ denv/teacher/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blood_group(models.Model) :
+   blood_group = models.CharField(max_length=3)
+
+   def __str__(self):
+       return self.blood_group
+
+   class Meta :
+      ordering = ['blood_group']
+   
